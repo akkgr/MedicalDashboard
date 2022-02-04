2baa2c62d2295c968f1f16e1aa1d7299c7e76591
tasos <akkell7@gmail.com>
2022-02-04 23:02:08 +0200

    wip

---
+++ Data/TSValue.cs
@@ -0,0 +1,9 @@
+namespace MedicalDashboard.Data;
+
+public class TSValue
+{
+    public DateTime? InDate { get; set; }
+    public Int64 MIN { get; set; }
+    public Int64 MID { get; set; }
+    public Int64 MAX { get; set; }
+}
